e78035bee832e4824dba932006835dd95dd7f9ae
SanRay240 <real.sanray240@gmail.com>
2025-02-27 15:15:35 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variables_Sakiestewa_Santino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int favoriteNumber:
+                bool isJumping
+        }
+    }
+}
